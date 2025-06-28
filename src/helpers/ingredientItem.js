e33b1dc2cb0487b2e115ca37aa1ae1e6fbf13db4
Lindy Roquemore <lindy.roquemore@icloud.com>
2025-06-28 09:44:42 -0500

    edit intro text

--- src/helpers/ingredientItem.js
+++ src/helpers/ingredientItem.js
@@ -4,7 +4,7 @@ ingredientItem.register = (Handlebars) => {
 	Handlebars.registerHelper("ingredientItem", (obj) => {
 		if (obj == null || obj === undefined) return "";
 		const { amount, name, note } = obj;
-		return `› ${amount[0].quantity} ${amount[0].unit}${amount[1] ? ` (${amount[1].quantity} ${amount[1].unit})` : ""} ${name}${note ? ` (${note})` : ""}`;
+		return `▸ ${amount[0].quantity} ${amount[0].unit}${amount[1] ? ` (${amount[1].quantity} ${amount[1].unit})` : ""} ${name}${note ? ` (${note})` : ""}`;
 	});
 };
 
